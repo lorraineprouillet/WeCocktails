60fbf173183aee6367c03eecf95a60c5345f1c33
Rosanna Guttadoro <rosanna.guttadoro@efpedu.fr>
2021-03-31 15:57:49 +0200

    modifi

--- app/src/main/java/com/example/wecocktails/PageSelect.java
+++ app/src/main/java/com/example/wecocktails/PageSelect.java
@@ -10,5 +10,11 @@ public class PageSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_select);
+
+
+
+
+
+        //poolparty
     }
 }
