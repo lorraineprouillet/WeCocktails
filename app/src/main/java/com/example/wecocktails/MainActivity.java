7b6e398f37295044e1896b0998637372dfd1c52d
Rosanna Guttadoro <rosanna.guttadoro@efpedu.fr>
2021-04-07 14:40:16 +0200

    modifications

--- app/src/main/java/com/example/wecocktails/MainActivity.java
+++ app/src/main/java/com/example/wecocktails/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity<lateinit, adapter> extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        adapter = ArrayAdapter( context: this,android.R.layout.simple_list_item_1, resources.GetStringArray(R.array.ListCocktails))
 
 
         boutonDemarrer = (Button) findViewById(R.id.button);
--- app/src/main/java/com/example/wecocktails/PageRecherche.java
+++ app/src/main/java/com/example/wecocktails/PageRecherche.java
@@ -7,13 +7,16 @@ import android.widget.ArrayAdapter;
 
 public class PageRecherche extends AppCompatActivity {
 
-
+    private ArrayAdapter adapter ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_recherche);
+
+        adapter = ArrayAdapter( this,android.R.layout.simple_list_item_1, resources.GetStringArray(R.array.ListCocktails));
+
 
 
     }
